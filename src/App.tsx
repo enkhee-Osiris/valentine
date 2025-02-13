0b616e6403b2ddab29e6928b3b308fdcdad0edef
enkhee-Osiris <enkhee.ag@gmail.com>
2025-02-13 21:09:34 +0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Box } from "./components/Box";
+
+function App() {
+  return (
+    <Canvas>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+}
+
+export default App;
